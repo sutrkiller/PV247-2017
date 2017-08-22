768c8b495a3fc972341faa2eaa2e7d0f37f8a013
zuzanad <zuzanad@kentico.com>
2017-08-22 12:53:23 +0200

    Redux: Introduce action types and action creators for items list

---
+++ DemoReactApp/src/actions/todo-list/actionCreators.js
@@ -0,0 +1,42 @@
+import {uuid} from '../../utils/uuidGenerator';
+import {
+    TODO_LIST_ITEM_CREATE,
+    TODO_LIST_ITEM_DELETE,
+    TODO_LIST_ITEM_MOVE,
+    TODO_LIST_ITEM_UPDATE
+} from '../../constants/actionTypes';
+
+export const createNewItem = (newItem) => ({
+    type: TODO_LIST_ITEM_CREATE,
+    payload: {
+        item: {
+            ...newItem,
+            // note that we have an explicit dependency and will refactor this
+            // to use dependency injection in following commits
+            id: uuid(),
+        },
+    }
+});
+
+export const updateItem = (item) => ({
+    type: TODO_LIST_ITEM_UPDATE,
+    payload: {
+        item,
+    }
+});
+
+export const deleteItem = (id) => ({
+    type: TODO_LIST_ITEM_DELETE,
+    payload: {
+        id,
+    }
+});
+
+export const moveItem = (moveItemId, destinationItemId) => ({
+    type: TODO_LIST_ITEM_MOVE,
+    payload: {
+        moveItemId,
+        destinationItemId,
+    }
+});
+
