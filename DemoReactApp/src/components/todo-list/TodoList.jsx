ba2d395eb461c7c9810f1b3dfe0c6c8fe15189ed
PetrS2 <PetrS2@kentico.com>
2017-10-11 09:39:50 +0200

    Update to React@16.0.0

--- DemoReactApp/src/components/todo-list/TodoList.jsx
+++ DemoReactApp/src/components/todo-list/TodoList.jsx
@@ -10,59 +10,77 @@ import { ButtonRow } from './TodoList.styles';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
 import CSSTransition from 'react-transition-group/CSSTransition';
 
-function TodoList(props) {
-    let itemElements = props.list.map(item =>
-            item.id === props.editedItemId ? (
-                <CSSTransition key={`edited-${item.id}`} timeout={{enter: 250, exit: 150}} classNames="edited-item" >
+class TodoList extends React.PureComponent {
+    static propTypes = {
+        list: PropTypes.instanceOf(Immutable.List).isRequired,
+        editedItemId: PropTypes.string,
+        createNewFormVisible: PropTypes.bool,
+        isDragging: PropTypes.bool,
+        onCreateNewClick: PropTypes.func.isRequired,
+    };
+
+    render() {
+        let itemElements = this.props.list.map(item =>
+            item.id === this.props.editedItemId ? (
+                <CSSTransition key={`edited-${item.id}`}
+                               timeout={{
+                                   enter: 250,
+                                   exit: 150
+                               }}
+                               classNames="edited-item">
                     <TodoListEditedItem key={item.id} item={item} submitButtonText="Save" />
                 </CSSTransition>
             ) : (
-                <CSSTransition key={`bar-${item.id}`} timeout={{enter: 150, exit: 0}} classNames="bar-item">
+                <CSSTransition key={`bar-${item.id}`}
+                               timeout={{
+                                   enter: 150,
+                                   exit: 0
+                               }}
+                               classNames="bar-item">
                     <TodoListBarItem key={item.id} item={item} />
                 </CSSTransition>
             )
-    );
+        );
 
-    if (props.createNewFormVisible) {
-        itemElements = itemElements.push((
-            <CSSTransition key="new-item" timeout={{ enter: 350, exit: 150}} classNames="new-item">
-                <TodoListNewItem />
-            </CSSTransition>
-        ));
-    }
+        if (this.props.createNewFormVisible) {
+            itemElements = itemElements.push((
+                <CSSTransition key="new-item"
+                               timeout={{
+                                   enter: 350,
+                                   exit: 150
+                               }}
+                               classNames="new-item">
+                    <TodoListNewItem />
+                </CSSTransition>
+            ));
+        }
 
-    return (
-        <div>
-            <div className="row">
-                <div className="col-xs-12 col-sm-9 col-md-6">
-                    <TransitionGroup>
-                        {itemElements}
-                    </TransitionGroup>
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-12 col-sm-9 col-md-6">
+                        <TransitionGroup>
+                            {itemElements}
+                        </TransitionGroup>
+                    </div>
                 </div>
+                <ButtonRow className="row">
+                    <div className="col-xs-12 col-sm-9 col-md-6">
+                        {!this.props.createNewFormVisible ?
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                onClick={this.props.onCreateNewClick}
+                            >
+                                Create new
+                            </button> : null}
+                    </div>
+                </ButtonRow>
             </div>
-            <ButtonRow className="row">
-                <div className="col-xs-12 col-sm-9 col-md-6">
-                    {!props.createNewFormVisible ?
-                        <button
-                            type="button"
-                            className="btn btn-primary"
-                            onClick={props.onCreateNewClick}
-                        >
-                            Create new
-                        </button> : null}
-                </div>
-            </ButtonRow>
-        </div>
-    );
+        );
+    }
 }
 
-TodoList.propTypes = {
-    list: PropTypes.instanceOf(Immutable.List).isRequired,
-    editedItemId: PropTypes.string,
-    createNewFormVisible: PropTypes.bool,
-    isDragging: PropTypes.bool,
-    onCreateNewClick: PropTypes.func.isRequired,
-};
-
 const DndTodoList = DragDropContext(HTML5Backend)(TodoList);
+
 export { DndTodoList as TodoList };
